ac9a41292b35e842dadd0a0a65a9cb04d0ab6cc5
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-11 21:33:13 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ require "pry"
 def alphabetize(arr)
   # code here
   espr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  alpha = "abcdefghijklmnopqrstuvwxyz12"
+  alpha = "abcdefghijklmnopqrstuvwxyz0*"
 
   arr.sort_by do |string|
 
