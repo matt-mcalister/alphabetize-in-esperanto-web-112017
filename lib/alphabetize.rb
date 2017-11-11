dea2fe5f447da8f77de6ef9b4c279b9725684d8e
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-11 21:32:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,10 +3,8 @@ require "pry"
 def alphabetize(arr)
   # code here
   espr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  alpha = "abcdefghijklmnopqrstuvwxyz, "
 
   arr.sort_by do |string|
-    string.tr(espr,alpha)
-    # binding.pry
+    string.find {|el| espr.include?(el)}      
   end
 end
