9b2ec7ccf93e7843d7f46cd11f5e2323dbfa7da2
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-11 21:22:28 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  espr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alpha = "abcdefghijklmnopqrstuvwxyz,."
+
+  arr.sort_by do |string|
+    string.tr(espr,alpha)
+  end
 end
